e11da1fc327417b172174150ac40778de678f004
caioanizer <caioanizer@gmail.com>
2025-04-15 23:46:37 +0000

    first commit

---
+++ lib/ui/_core/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color backgroundColor = Color(0xFF202123);
+  static const Color maincolor = Color(0xFFffa559);
+}
