d7c3672db6ad2ac07caf23e51d3fcbdd2f30f364
caioanizer <caioanizer@gmail.com>
2025-04-21 23:31:10 +0000

    implementação da sacola e tela de pedidos

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,39 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/model/restaurant.dart';
+import 'package:myapp/ui/_core/widgets/appbar.dart';
 import 'package:myapp/ui/data/categories_data.dart';
+import 'package:myapp/ui/data/restaurant_data.dart';
 import 'package:myapp/ui/home/widgets/category_widget.dart';
+import 'package:provider/provider.dart';
+import 'package:myapp/ui/home/widgets/restaurant_widget.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    RestaurantData restaurantData = Provider.of<RestaurantData>(context);
     return Scaffold(
       drawer: Drawer(),
-      appBar: AppBar(
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
-        ],
-       
-      ),
-      body: Padding(padding: EdgeInsets.symmetric(horizontal: 24.0), child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        spacing: 32.0,
-        children: [
-          Image.asset('assets/logo.png', width: 147,),
-          Text("Boas-vindas!"),
-          TextFormField(),
-          Text("Escolha por categoria"),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              spacing: 8.0,
-              children: List.generate(CategoriesData.listCategories.length, (index) {
-              return CategoryWidget(category: CategoriesData.listCategories[index]);
-            }),),
-          ),
-          Image.asset('assets/banners/banner_promo.png'),
-          Text("Bem avaliados"),
-        ],
+      appBar: getAppBar(context: context),
+      body: Padding(padding: EdgeInsets.symmetric(horizontal: 24.0), 
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 32.0,
+          children: [
+            Image.asset('assets/logo.png', width: 147,),
+            Text("Boas-vindas!"),
+            TextFormField(),
+            Text("Escolha por categoria"),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                spacing: 8.0,
+                children: List.generate(CategoriesData.listCategories.length, (index) {
+                return CategoryWidget(category: CategoriesData.listCategories[index]);
+              }),),
+            ),
+            Image.asset('assets/banners/banner_promo.png'),
+            Text("Bem avaliados"),
+            Column(
+              spacing: 16.0,
+              children: List.generate(restaurantData.listRestaurant.length, (index){
+
+                Restaurant restaurant = restaurantData.listRestaurant[index];
+                return RestaurantWidget(restaurant: restaurant);
+              }),
+            ),
+            SizedBox(height: 64.0),
+          ],
+        ),
       ),),
     );
   }
